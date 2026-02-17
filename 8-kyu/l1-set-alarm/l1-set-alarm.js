b5a66fd6632aef26ab8fbdf9dabd9fca76bbdf2d
Error404notf <104951634+Error404notf@users.noreply.github.com>
2026-02-17 21:26:54 +0300

    Add solution - CodeHub

---
+++ 8-kyu/l1-set-alarm/l1-set-alarm.js
@@ -0,0 +1,7 @@
+// function setAlarm(employed, vacation){
+//   return employed > vacation;
+// }
+​
+// const setAlarm = (employed, vacation) => employed && vacation == false ? true : false
+​
+const setAlarm = (employed, vacation) => employed === true && vacation === false ? true : false
