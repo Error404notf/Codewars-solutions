f8c6d7e44915de9f9ab823cd2390bcae41f916c9
Error404notf <104951634+Error404notf@users.noreply.github.com>
2026-02-24 11:53:42 +0300

    Add solution - CodeHub

---
+++ 8-kyu/grasshopper-messi-goals-function/grasshopper-messi-goals-function.js
@@ -0,0 +1,7 @@
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  // goals in all leagues
+  // laLigaGoals, copaDelReyGoals, championsLeagueGoals
+  // add all goals in all three goals
+  return Number(laLigaGoals + copaDelReyGoals + championsLeagueGoals)
+  // return total
+}
