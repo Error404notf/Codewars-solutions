0b2232bc25bd5973a76ea1c956cba1c1fb1d61ef
Error404notf <104951634+Error404notf@users.noreply.github.com>
2026-02-27 15:06:04 +0300

    Add solution - CodeHub

---
+++ 8-kyu/parse-nice-int-from-char-problem/parse-nice-int-from-char-problem.js
@@ -0,0 +1,8 @@
+function getAge(inputString){
+// return the girl's correct age as an integer. Happy coding :) 
+  // return girls age from the string
+  // split the string
+  // extract the first  word
+  // return as integer
+   return Number(inputString[0])
+}
