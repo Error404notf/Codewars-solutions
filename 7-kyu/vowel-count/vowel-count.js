2aef6a6b31fc84bb59009d9e8b25039fa5b6edfb
Error404notf <104951634+Error404notf@users.noreply.github.com>
2026-03-01 22:57:45 +0300

    Update solution - CodeHub

--- 7-kyu/vowel-count/vowel-count.js
+++ 7-kyu/vowel-count/vowel-count.js
@@ -1,27 +1,3 @@
-function getCount(str) {
-// //   Parameters => str
-// //   Return => number of vowels in a given string
-// //   Examples => ('abra') returns 2 vowels
-// //   Pseudocode
-//     // extract the string
-//   // count the number the vowels in a given string
-//   // return the number of vowels
-  let vowelscount = 0;
-  const vowels = ['a','e','i','o','u']
-  for(let i = 0; i < str.length; i ++){
-    if(vowels.includes(str[i])){
-      vowelscount++
-    }
-  }
-  return vowelscount;
-}
-​
-// const getCount = str => {
-// //   Parameters = str
-// //   Result = return count of the number of vowels in a given string
-// //   Examples = abra outputs 2
-// //   Pseudocode 
-//   // initialize a variable for the vowelscount
 //   // extract the string by looping through it
 //   // count the number of vowels
 //   // store it in the variable
@@ -34,3 +10,22 @@ function getCount(str) {
 //   }
 //   return vowelscount;
 // }
+​
+const getCount = str => {
+// //   Parameters = str
+// //   Result = return count of the number of vowels in a given string
+// //   Examples = abra outputs 2
+// //   Pseudocode 
+//   // initialize a variable for the vowelscount
+//   // extract the string by looping through it
+//   // count the number of vowels
+//   // store it in the variable
+//   // return the variable
+  let vowelscount = 0;
+  for(let i = 0; i < str.length; i++){
+    if('aeiou'.includes(str[i])){
+      vowelscount += 1;
+    }
+  }
+  return vowelscount;
+}
