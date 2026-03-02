ec5e627cecfe7f18bf87514ad77a29f0fc3b660c
Error404notf <104951634+Error404notf@users.noreply.github.com>
2026-03-02 12:53:23 +0300

    Add solution - CodeHub

---
+++ 8-kyu/grasshopper-check-for-factor/grasshopper-check-for-factor.js
@@ -0,0 +1,10 @@
+function checkForFactor (base, factor) {
+  // Parameter = base and factor
+  //Return = if base and factor is % true or false
+  //Example =  (6,3) true (4,5) false
+  //Pseudocode 
+     // extract base and factor
+     // if base % factor == 0 ? true : false
+  
+   return base % factor == 0 ? true : false
+}
