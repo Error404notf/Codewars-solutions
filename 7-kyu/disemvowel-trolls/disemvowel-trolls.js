e44002e0c735cd5dfea092cb96da98b4429177dc
Error404notf <104951634+Error404notf@users.noreply.github.com>
2026-03-02 13:44:24 +0300

    Add solution - CodeHub

---
+++ 7-kyu/disemvowel-trolls/disemvowel-trolls.js
@@ -0,0 +1,13 @@
+function disemvowel(str) {
+  // Parameters str
+  //Return new word with no vowels
+  //Examples
+  //Pseudocode
+  
+  //retrieve a string
+  
+  // loop through the string
+  // remove vowels
+  // display the new word
+  return str.replace(/[aeiouAEIOU]/g, '')
+}
