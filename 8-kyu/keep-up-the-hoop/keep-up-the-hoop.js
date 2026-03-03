5e89ef6872f031332b77d60f200f8981fdff9495
Error404notf <104951634+Error404notf@users.noreply.github.com>
2026-03-03 11:15:57 +0300

    Add solution - CodeHub

---
+++ 8-kyu/keep-up-the-hoop/keep-up-the-hoop.js
@@ -0,0 +1,10 @@
+function hoopCount (n) {
+   //Parameter = n
+  // Return =if 10 or more hoops "Great, now move on to tricks" if less than 10 "Keep at it until you get it".
+  // Examples (3) "Keep at it until you get it" , (11),"Great, now move on to tricks"
+  //Pseudocode
+  
+  // check the number
+  // if n is greater or = 10 "Great, now move on to tricks". else "Keep at it until you get it".
+  return n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it"
+}
