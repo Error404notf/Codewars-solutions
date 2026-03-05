c276875854b18fb0078ce6779eadf6afe4a9d429
Error404notf <104951634+Error404notf@users.noreply.github.com>
2026-03-05 15:53:35 +0300

    Update solution - CodeHub

--- 8-kyu/array-plus-array/array-plus-array.js
+++ 8-kyu/array-plus-array/array-plus-array.js
@@ -1,15 +1,17 @@
-function arrayPlusArray(arr1,arr2) {
-//   return arr1 + arr2; //something went wrong
+// function arrayPlusArray(arr1,arr2) {
+// //   return arr1 + arr2; //something went wrong
   
-  //Parameters = arr1 , arr2
-  //Return = sum of the two arrays
-  //Examples = ([1, 2, 3], [4, 5, 6]), 21)
-  //Pseudocode
+//   //Parameters = arr1 , arr2
+//   //Return = sum of the two arrays
+//   //Examples = ([1, 2, 3], [4, 5, 6]), 21)
+//   //Pseudocode
   
-  // add the sum of each array element first .reduce
-  // add both of them together i think it will work
-  let arrone = arr1.reduce((a,b) => a + b, 0)
-  let arrtwo = arr2.reduce((a,b) => a + b , 0)
+//   // add the sum of each array element first .reduce
+//   // add both of them together i think it will work
+//   let arrone = arr1.reduce((a,b) => a + b, 0)
+//   let arrtwo = arr2.reduce((a,b) => a + b , 0)
 ​
-  return arrone + arrtwo;
-}
+//   return arrone + arrtwo;
+// }
+​
+const arrayPlusArray = (arr1,arr2) => arr1.concat(arr2).reduce((a,b) => a + b , 0)
