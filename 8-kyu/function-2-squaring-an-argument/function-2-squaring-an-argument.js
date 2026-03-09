c7b2d41e2ceba4e8adf5cc2d8735d72134d6c675
Error404notf <104951634+Error404notf@users.noreply.github.com>
2026-03-09 13:00:46 +0300

    Add solution - CodeHub

---
+++ 8-kyu/function-2-squaring-an-argument/function-2-squaring-an-argument.js
@@ -0,0 +1,11 @@
+// Write the "square"-function here
+//Parameter =  argument
+//Return = square of the argument
+//Example = square(3), 9
+//Pseudocode
+​
+// write the function
+// write the function call and the argument that genearates a squared argument
+const square = n => n **2;
+​
+​
