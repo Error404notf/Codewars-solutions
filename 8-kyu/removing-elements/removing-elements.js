d58e8417d86996571eea1d890858b6eefbf271fc
Error404notf <104951634+Error404notf@users.noreply.github.com>
2026-03-10 14:59:25 +0300

    Update solution - CodeHub

--- 8-kyu/removing-elements/removing-elements.js
+++ 8-kyu/removing-elements/removing-elements.js
@@ -1,16 +1,21 @@
-function removeEveryOther(arr){
-  //Parameter = arr
-  //Return = remove an arr with the second element removed
-  //Examples = [['Goodbye'], {'Great': 'Job'}]),[['Goodbye']]
-  //Pseudocode
+// function removeEveryOther(arr){
+//   //Parameter = arr
+//   //Return = remove an arr with the second element removed
+//   //Examples = [['Goodbye'], {'Great': 'Job'}]),[['Goodbye']]
+//   //Pseudocode
   
-  //extract the array first
-  // remove the second element then the 4th element ...
-  // return the new array 
+//   //extract the array first
+//   // remove the second element then the 4th element ...
+//   // return the new array 
   
-  let removearr = []
-  for(let i = 0; i < arr.length; i = i + 2){
-    removearr.push(arr[i])
-  }
-  return removearr;
+//   let removearr = []
+//   for(let i = 0; i < arr.length; i = i + 2){
+//     removearr.push(arr[i])
+//   }
+//   return removearr;
+// }
+​
+const removeEveryOther = arr => {
+  return arr.filter((_,index) => !(index % 2))
+  return arr.filter((element,index) => index % 2 === 0)
 }
