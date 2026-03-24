a26fb18a79fadbf41a5b70af8fb65fa8d02c550b
Error404notf <104951634+Error404notf@users.noreply.github.com>
2026-03-24 23:07:24 +0300

    Add solution - CodeHub

---
+++ 8-kyu/grasshopper-debug-sayhello/grasshopper-debug-sayhello.js
@@ -0,0 +1,10 @@
+function sayHello(name) {
+  return `Hello, ${name}`
+  return 'Hello,' + ' ' + name;
+  //Parameters = name
+  //Return = return hello with person's name
+  //Examples = sayHello('Mr. Spock'), 'Hello, Mr. Spock'
+  //Pseudocode
+  // get the name of the person
+  //return hello + name
+}
