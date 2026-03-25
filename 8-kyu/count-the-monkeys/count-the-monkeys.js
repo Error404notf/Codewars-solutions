6b9681117cef0d354a2127eeab3010dc51814956
Error404notf <104951634+Error404notf@users.noreply.github.com>
2026-03-25 17:20:25 +0300

    Update solution - CodeHub

--- 8-kyu/count-the-monkeys/count-the-monkeys.js
+++ 8-kyu/count-the-monkeys/count-the-monkeys.js
@@ -5,9 +5,14 @@ function monkeyCount(n) {
   //Pseudocode
   // create a variable for an array
   // loop from 1 till n
-  let totalnumber = []
-  for(let i = 1; i <=n; i++){
-    totalnumber.push(i)
-  }
-  return totalnumber;
+//   let totalnumber = []
+//   for(let i = 1; i <=n; i++){
+//     totalnumber.push(i)
+//   }
+//   return totalnumber;
+  
+  // or do it this way
+    for (var i = 0, arr = []; i <n; arr.push(++i));
+  
+    return arr;
 }
