5f05798e2519a601f786be4b8f1fdd15e658d8b2
Error404notf <104951634+Error404notf@users.noreply.github.com>
2026-04-12 13:40:39 +0300

    Add solution - CodeHub

---
+++ 8-kyu/sum-the-strings/sum-the-strings.js
@@ -0,0 +1,12 @@
+function sumStr(a,b) {
+   //Parameters = a,b
+  //Return = return the sum of the parameters as strings
+  //Examples  = (sumStr("4","5"), "9")
+  //Pseudocode
+  // convert the two parameters into numbers
+  // add them  up then convert to string
+//   let convertA = Number(a)
+//   let convertB = Number(b)
+//   return String(convertA + convertB)
+  return (+(a) + +(b)).toString()
+}
