c8788e06012f2cd35ba565c6becdaa73de19d216
Error404notf <104951634+Error404notf@users.noreply.github.com>
2026-04-26 16:17:01 +0300

    Add solution - CodeHub

---
+++ 8-kyu/difference-of-volumes-of-cuboids/difference-of-volumes-of-cuboids.js
@@ -0,0 +1,14 @@
+function findDifference(a, b) {
+  //loading...
+  //Parameter = a,b
+  //Return = difference in volumes between the two arrays
+  //Examples = findDifference([11, 2, 5], [1, 10, 8]), 30
+  //Pseudocode
+  // find the volume of each array
+  // find the difference
+  let volume = a[0] * a[1] * a[2]
+  let volumeTwo = b[0] * b[1] * b[2]
+  let highestNumber = Math.max(volume,volumeTwo)
+  let lowestNumber = Math.min(volume,volumeTwo)
+  return highestNumber - lowestNumber;
+}
