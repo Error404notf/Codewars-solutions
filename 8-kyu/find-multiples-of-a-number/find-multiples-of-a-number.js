0ad069a08c1079cdc206b43b18acb9a32d6536db
Error404notf <104951634+Error404notf@users.noreply.github.com>
2026-05-02 11:56:30 +0300

    Update solution - CodeHub

--- 8-kyu/find-multiples-of-a-number/find-multiples-of-a-number.js
+++ 8-kyu/find-multiples-of-a-number/find-multiples-of-a-number.js
@@ -7,9 +7,17 @@ function findMultiples(integer, limit) {
   //loop from 1 till limit
   // the integer should <= limit
  const arr = []
- for(let i = 1; i <= limit; i++){
-  if(integer * i <= limit){
-    arr.push(integer * i)
+//  for(let i = integer; i <= limit; i+=integer){
+ 
+//     arr.push(i)
+    
+//   }
+//   return arr;
+  
+  //Or implement it this way 
+  for(let i = 1; i <=limit; i++){
+    if(integer * i <= limit){
+      arr.push(integer * i)
     }
   }
   return arr;
