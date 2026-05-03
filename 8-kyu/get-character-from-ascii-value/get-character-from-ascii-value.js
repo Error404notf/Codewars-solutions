6f525c17d4f6f772dd78b294e191aec5623cf92f
Error404notf <104951634+Error404notf@users.noreply.github.com>
2026-05-03 15:20:03 +0300

    Add solution - CodeHub

---
+++ 8-kyu/get-character-from-ascii-value/get-character-from-ascii-value.js
@@ -0,0 +1,8 @@
+function getChar(c){
+  //Parameters = c
+  //Return = the ascii xter
+  //Examples = (getChar(63),'?')
+  //Pseudocode
+  // convert c to ascii value and return it
+  return String.fromCharCode(c)
+}
