ea8821b81a61f04a463c3ec255e7d27ea0e5bf4a
Error404notf <104951634+Error404notf@users.noreply.github.com>
2026-05-08 21:00:17 +0300

    Add solution - CodeHub

---
+++ 8-kyu/plural/plural.js
@@ -0,0 +1,8 @@
+function plural(n) {
+  //Parameter = n
+  //Return = false for 1 and the rest is true
+  //Examples = (0), true, "Plural for 0"
+  //Pseudocode
+  // if n == 1 it will be false for other numbers it will be true
+  return n === 1 ? false : true
+}
