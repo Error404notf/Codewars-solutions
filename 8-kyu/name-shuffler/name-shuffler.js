ec8ac319c8573426c11525aa0a1ab05066d4084f
Error404notf <104951634+Error404notf@users.noreply.github.com>
2026-05-11 11:52:17 +0300

    Add solution - CodeHub

---
+++ 8-kyu/name-shuffler/name-shuffler.js
@@ -0,0 +1,9 @@
+function nameShuffler(str){
+  //Parameter = str
+  //Return = reversed word
+  //Examples = ('john McClane'),'McClane john')
+  //Pseudocode
+  //split the str, reverse then join
+  return str.split(' ').reverse().join(' ')
+   
+}
