861e86261a1e3974802f622080a05f3a5ea23d08
Error404notf <104951634+Error404notf@users.noreply.github.com>
2026-05-18 12:37:21 +0300

    Add solution - CodeHub

---
+++ 7-kyu/sum-of-odd-numbers/sum-of-odd-numbers.js
@@ -0,0 +1,12 @@
+function rowSumOddNumbers(n) {
+  // TODO
+//  const tsplit = n.toString().split('')
+//  const total = tsplit.reduce((a,b)=> Number(a) + Number(b))
+//  return total
+  //Parameters = n
+  //Return = sum based off the row (N)
+  //Examples = rowSumOddNumbers(42), 74088
+  //Pseudocode
+  // sumation of numbers in a specific row
+  return n*n*n;
+}
