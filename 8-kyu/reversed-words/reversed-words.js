4e562054d58872afcabaec548a45afd51312f4e2
Error404notf <104951634+Error404notf@users.noreply.github.com>
2026-05-20 18:36:18 +0300

    Add solution - CodeHub

---
+++ 8-kyu/reversed-words/reversed-words.js
@@ -0,0 +1,11 @@
+function reverseWords(str){
+  // reverse those words
+  //Parameters = str
+  //Return = reversed words
+  //Examples = ("hello world!"),  "world! hello")
+  //Pseudocode
+  // split the words to an array
+  //reverse the words
+  // join with spacing done
+  return str.split(' ').reverse().join(' ')
+}
