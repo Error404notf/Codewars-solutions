a0c848540e9032543b9a4b7baa658cc330be3a62
Error404notf <104951634+Error404notf@users.noreply.github.com>
2026-05-28 18:04:14 +0300

    Add solution - CodeHub

---
+++ 7-kyu/string-ends-with/string-ends-with.js
@@ -0,0 +1,8 @@
+function solution(str, ending){
+  // Parameters = str, ending
+  // Return = true = str ends with ending
+  //Examples = 'abcde', 'cde', true 
+  //Pseudocode
+  // if str.endsWith(ending) ? true : false
+  return str.endsWith(ending) ? true : false
+}
