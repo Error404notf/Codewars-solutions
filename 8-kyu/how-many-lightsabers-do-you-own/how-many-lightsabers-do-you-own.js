52954d2defca42d8159838c8ed590a2cf8e268b3
Error404notf <104951634+Error404notf@users.noreply.github.com>
2026-06-03 11:40:56 +0300

    Add solution - CodeHub

---
+++ 8-kyu/how-many-lightsabers-do-you-own/how-many-lightsabers-do-you-own.js
@@ -0,0 +1,8 @@
+function howManyLightsabersDoYouOwn(name) {
+  //Parameters = name
+  //Return = number of lightsabers only Zach has 18, everyone else has 0 
+  //Examples = ( "Adam"), 0 ) ("Zach"), 18 ) 
+  //Pseudocode
+  // if name == 'Zach' return 18 else return 0
+  return name == 'Zach' ? 18 : 0
+} 
