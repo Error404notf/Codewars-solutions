3f79f8f41d24965ea0472e3517e3aa5cbab45fe8
Error404notf <104951634+Error404notf@users.noreply.github.com>
2026-06-15 15:47:48 +0300

    Add solution - CodeHub

---
+++ 8-kyu/find-numbers-which-are-divisible-by-given-number/find-numbers-which-are-divisible-by-given-number.js
@@ -0,0 +1,17 @@
+function divisibleBy(numbers, divisor){
+  //Parameter = numbers, divisor
+  //Return = numbers divisible by the divisor inclusive of 0, else return an empty arry
+  //Examples = [0,1,2,3,4,5,6], 4), [0,4]
+  //Pseudocode
+  // loop through the array
+  // if number % divisor = push it to the new array if not empty array
+  let newArr = []
+  for(let i = 0; i<numbers.length; i++){
+    
+    if(numbers[i] % divisor == 0){
+      newArr.push(numbers[i])
+    }
+    
+}
+   return newArr;
+}
