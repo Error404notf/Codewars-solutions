1bd23414dcd3439640051d9ce86a1b103bc517e8
Error404notf <104951634+Error404notf@users.noreply.github.com>
2026-06-26 15:40:03 +0300

    Add solution - CodeHub

---
+++ 8-kyu/training-js-number-1-create-your-first-js-function-and-print-hello-world/training-js-number-1-create-your-first-js-function-and-print-hello-world.js
@@ -0,0 +1,10 @@
+//refer to the example and write your first JS function
+//Parameters = function, function name
+//Return = hello world
+//Examples = 
+//Pseudocode
+// write a function function name is 'hello world' let str = 'hello world'
+  function helloWorld(){
+    let str = 'Hello World!'
+    console.log(str)
+  }
