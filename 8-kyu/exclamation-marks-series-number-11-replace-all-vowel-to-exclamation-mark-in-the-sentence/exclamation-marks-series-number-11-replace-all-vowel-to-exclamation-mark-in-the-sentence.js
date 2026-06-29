16e50371b888e23022e399f19c6c349dc8b5bc67
Error404notf <104951634+Error404notf@users.noreply.github.com>
2026-06-29 22:00:31 +0300

    Update solution - CodeHub

--- 8-kyu/exclamation-marks-series-number-11-replace-all-vowel-to-exclamation-mark-in-the-sentence/exclamation-marks-series-number-11-replace-all-vowel-to-exclamation-mark-in-the-sentence.js
+++ 8-kyu/exclamation-marks-series-number-11-replace-all-vowel-to-exclamation-mark-in-the-sentence/exclamation-marks-series-number-11-replace-all-vowel-to-exclamation-mark-in-the-sentence.js
@@ -4,5 +4,7 @@ function replace(s) {
   //Examples = "aeiou", "!!!!!"
   //Pseudocode
   // replace vowels with !!
-    return s.replace(/a/g, '!').replace(/e/g,'!').replace(/i/g, '!').replace(/o/g,'!').replace(/u/g, '!').replace(/A/g, '!').replace(/E/g,'!').replace(/I/g, '!').replace(/O/g,'!').replace(/U/g, '!');
+//     return s.replace(/a/g, '!').replace(/e/g,'!').replace(/i/g, '!').replace(/o/g,'!').replace(/u/g, '!').replace(/A/g, '!').replace(/E/g,'!').replace(/I/g, '!').replace(/O/g,'!').replace(/U/g, '!');
+  // ANOTHER IMPLEMENTATION
+  return s.replace(/[aeiou]/ig, '!')
 }
