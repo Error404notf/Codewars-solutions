f19d58ba1d3a633dc3a117857d8561005ec41865
Error404notf <104951634+Error404notf@users.noreply.github.com>
2026-06-29 21:58:50 +0300

    Add solution - CodeHub

---
+++ 8-kyu/exclamation-marks-series-number-11-replace-all-vowel-to-exclamation-mark-in-the-sentence/exclamation-marks-series-number-11-replace-all-vowel-to-exclamation-mark-in-the-sentence.js
@@ -0,0 +1,8 @@
+function replace(s) {
+  //Parameters = s
+  //Return = !! to replace vowels
+  //Examples = "aeiou", "!!!!!"
+  //Pseudocode
+  // replace vowels with !!
+    return s.replace(/a/g, '!').replace(/e/g,'!').replace(/i/g, '!').replace(/o/g,'!').replace(/u/g, '!').replace(/A/g, '!').replace(/E/g,'!').replace(/I/g, '!').replace(/O/g,'!').replace(/U/g, '!');
+}
